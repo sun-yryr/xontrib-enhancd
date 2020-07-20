b934c65c2ce1531f8308b71c22df22d0f9f8f486
sun-yryr <taittide@gmail.com>
2020-07-20 16:40:03 +0900

    こんな感じに書けばいけそう

---
+++ test/mock_data.py
@@ -0,0 +1,11 @@
+from unittest.mock import patch, MagicMock
+
+__xonsh__ = {
+    "env": {
+
+    },
+    "execer": lambda: 'sample'
+}
+mock_xonsh = MagicMock()
+mock_xonsh.__getitem__.side_effect = __xonsh__.__getitem__
+mock_aliases = MagicMock()
--- test/test_filepath.py
+++ test/test_filepath.py
@@ -1,8 +1,13 @@
 import unittest
-from xontrib.enhancd_lib import filepath
+from unittest.mock import patch
+from .mock_data import mock_xonsh, mock_aliases
 
-class TestFilePath(unittest.TestCase): 
+
+class TestFilePath(unittest.TestCase):
+    @patch('builtins.aliases', new=mock_aliases, create=True)
+    @patch('builtins.__xonsh__', new=mock_xonsh, create=True)
     def test_split_filterlist(self):
+        from xontrib.enhancd_lib import filepath
         i = 'peco:fzf:c'
         o = filepath.split_filterlist(i)
         self.assertTrue(o)
