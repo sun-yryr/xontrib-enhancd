68961b9c3a5eec2b750a209ec155bb6a09b9dde7
sun-yryr <taittide@gmail.com>
2020-07-20 18:38:06 +0900

    ?

--- test/test_filepath.py
+++ test/test_filepath.py
@@ -4,13 +4,23 @@ from .mock_data import mock_xonsh, mock_aliases
 
 
 class TestFilePath(unittest.TestCase):
-    @patch('builtins.aliases', new=mock_aliases, create=True)
-    @patch('builtins.__xonsh__', new=mock_xonsh, create=True)
-    def test_split_filterlist(self):
+    def setUp(self):
+        self.patcher_xonsh = patch('builtins.__xonsh__', new=mock_xonsh, create=True)
+        self.patcher_aliases = patch('builtins.aliases', new=mock_aliases, create=True)
+        self.mock_xonsh = self.patcher_xonsh.start()
+        self.mock_aliases = self.patcher_aliases.start()
+    
+    def tearDown(self):
+        self.patcher_xonsh.stop()
+        self.patcher_aliases.stop()
+    
+    @patch('xontrib.enhancd_lib.filepath.run')
+    def test_split_filterlist(self, mock):
         from xontrib.enhancd_lib import filepath
         i = 'peco:fzf:c'
         o = filepath.split_filterlist(i)
-        self.assertTrue(o)
+        self.assertIsNone(o)
+        self.assertEqual(len(i.split(':')), mock.call_count)
 
 if __name__ == "__main__":
     unittest.main()
